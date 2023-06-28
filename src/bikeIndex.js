63589f358a71c0a637f3e71f581ecbdbaa1b32c5
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-28 16:16:39 -0700

    Fix unicode date

--- src/bikeIndex.js
+++ src/bikeIndex.js
@@ -15,19 +15,41 @@ export default class BikeIndex {
         }
     }
 
-    static async getBikeByDateRange(location, startDate, endDate) {
+    static async getBikeByDateRange(location) {
         try {
             const response = await fetch(
-                `https://bikeindex.org:443/api/v3/search/count?location=${location}&distance=10&stolenness=proximity&date_stolen__gte=${startDate}&date_stolen__lte=${endDate}`
+                `https://bikeindex.org:443/api/v3/search?location=${location}&distance=10&stolenness=stolen`
             );
             const jsonifiedResponse = await response.json();
             if (!response.ok) {
                 const errorMessage = `${response.status} (${response.statusText})`;
                 throw new Error(errorMessage);
             }
-            return jsonifiedResponse;
+
+            if (jsonifiedResponse.bikes && jsonifiedResponse.bikes.length > 0) {
+                return { stolen: true, bikes: jsonifiedResponse.bikes };
+            } else {
+                return { stolen: false, bikes: [] };
+            }
         } catch (error) {
             return error;
+        }
+    }
+
+    static filterBikesByDateRange(bikes, startDate, endDate) {
+        return bikes.filter((bike) => {
+            const bikeDate = new Date(bike.date_stolen * 1000);
+            return bikeDate >= startDate && bikeDate <= endDate;
+        });
+    }
+    
+
+    static toUnixTimestamp(date) {
+        const timestamp = date.getTime();
+        if (!isNaN(timestamp)) {
+            return Math.floor(timestamp / 1000);
+        } else {
+            throw new Error('Invalid date format.');
         }
     }
 }
