5d468b9d7dd84475031676cae921a099277b1f99
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-28 14:01:10 -0700

    Add BikeIndex class

--- src/bikeIndex.js
+++ src/bikeIndex.js
@@ -1,18 +1,33 @@
-export default class BikeIndex{
-
-    static async getBikeByCount(city){
+export default class BikeIndex {
+    static async getBikeByCount(location) {
         try {
-            const response = await fetch(`https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=proximity`);
+            const response = await fetch(
+                `https://bikeindex.org:443/api/v3/search/count?location=${location}&stolenness=proximity`
+            );
             const jsonifiedResponse = await response.json();
             if (!response.ok) {
                 const errorMessage = `${response.status} (${response.statusText})`;
                 throw new Error(errorMessage);
             }
             return jsonifiedResponse;
-        } catch(error) {
+        } catch (error) {
             return error;
         }
     }
 
-
+    static async getBikeByDateRange(location, startDate, endDate) {
+        try {
+            const response = await fetch(
+                `https://bikeindex.org:443/api/v3/search/count?location=${location}&distance=10&stolenness=proximity&date_stolen__gte=${startDate}&date_stolen__lte=${endDate}`
+            );
+            const jsonifiedResponse = await response.json();
+            if (!response.ok) {
+                const errorMessage = `${response.status} (${response.statusText})`;
+                throw new Error(errorMessage);
+            }
+            return jsonifiedResponse;
+        } catch (error) {
+            return error;
+        }
+    }
 }
